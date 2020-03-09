be52812a740d475fc673f1abaa87d2248bdb8730
bennea27 <andrew.bennett07@bbc.co.uk>
2020-03-09 16:47:09 +0000

    Broken dangerouslySetInnerHTML

--- packages/components/psammead-idt1-include/src/index.jsx
+++ packages/components/psammead-idt1-include/src/index.jsx
@@ -1,20 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 
-// vendored in this library for demonstration purposes: https://www.npmjs.com/package/dangerously-set-html-content
-function DangerouslySetHtmlContent(props) {
-  const { html } = props;
-  const divRef = useRef(null);
-
-  useEffect(() => {
-    const slotHtml = document.createRange().createContextualFragment(html); // Create a 'tiny' document and parse the html string
-    divRef.current.innerHTML = ''; // Clear the container
-    divRef.current.appendChild(slotHtml); // Append the new content
-  }, [html]);
-
-  return <div ref={divRef}></div>;
-}
-
 const Idt1Include = ({ html, requireJsSrc }) => {
   return (
     <>
@@ -22,7 +8,7 @@ const Idt1Include = ({ html, requireJsSrc }) => {
         <script src={requireJsSrc} />
       </Helmet>
 
-      {<DangerouslySetHtmlContent html={html} />}
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </>
   );
 };
